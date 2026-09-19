e8ecbfe43c3d04183aed3e21f799b90d1d41fa3c
Râneer Almeida <raneerantonio@gmail.com>
2026-09-19 10:56:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lf-turma-sabado-v21';
+const CACHE_NAME = 'lf-turma-sabado-v22';
 
 // Arquivos que o app precisa baixar na primeira vez para funcionar offline
 const urlsToCache = [
